34ec7a5ea1c68672dcb26b5e464a2022a8b5cb39
jony <ravshanovjony7@gmail.com>
2025-06-19 09:46:08 +0500

    react assoslari

---
+++ my-app/src/components/app/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Sammi from '../sammi/sammi'
+
+function App() {
+  return (
+    <div className="App">
+      <Sammi/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/sammi/sammi.jsx
@@ -0,0 +1,9 @@
+import './sammi.css'
+
+const Sammi = () => {
+    return (
+        <h1>Hello world</h1>
+    )
+}
+
+export default Sammi
