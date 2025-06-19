bdaccdfb7bdbdcad48aa1e503c9ab65d2ed150d0
jony <ravshanovjony7@gmail.com>
2025-06-19 11:36:48 +0500

    4 dars jsx format

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/App';
 
+// JSX
+const el = (
+  <>
+    <h1>Hello Jony</h1>
+    <input type="text" />
+    <button type='button'>Click</button>
+  </>
+)
+
+// const el = React.createElement("h1", null, "Hello Joxa")
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  el
 );
