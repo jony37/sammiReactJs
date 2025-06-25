5002884e78563fa7fe2d167a402f658b7081d846
jony <ravshanovjony7@gmail.com>
2025-06-25 20:31:03 +0500

    13-dars Komponent xolati va class

--- sammi-app/src/components/app/App.js
+++ sammi-app/src/components/app/App.js
@@ -1,20 +1,73 @@
-const User = (props) => {
-  console.log(props);
+import { Component } from "react";
+import "../app/App.css";
 
-  return (
-    <div>
-      <h1>
-        Mening ismim {props.firstName}, Sharifim {props.lastName}
-      </h1>
-      <a href={props.link}>Youtube kanalim</a>
-    </div>
-  );
-};
+class User extends Component {
+  constructor(pros) {
+    super(pros);
+    this.state = {
+      count: 10,
+    };
+  }
+
+  incrFun = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decFun = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  zeroFun = () => {
+    this.setState({
+      count: this.state.count = 0,
+    });
+  };
+
+  render() {
+    const { firstName, lastName, link } = this.props;
+
+    return (
+      <div className="w-50 mx-auto">
+        <div className="border p-3 mt-5">
+          <h4>
+            Mening ismim {firstName}, sharifim {lastName}
+          </h4>
+          <a href={link}>Youtub kanalim</a>
+          <div className="mt-5">
+            <button
+              type="button"
+              className="btn btn-success btn-incr"
+              onClick={this.incrFun}
+            >
+              incriment
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={this.decFun}
+            >
+              dec
+            </button>
+            <button type="button" className="btn btn-warning" onClick={this.zeroFun}>
+              sbros
+            </button>
+          </div>
+          <p className="coutn">{this.state.count}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 const App = () => {
   return (
     <div>
       <User firstName="Jonpolat" lastName="Ravshanov" link="instagram.com" />
+      <User firstName="Jahongir" lastName="joxa" link="instagram.com" />
     </div>
   );
 };
