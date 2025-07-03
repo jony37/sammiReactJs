ab77d161f37accde7b0cfd5554532d76e7fdfe04
jony <ravshanovjony7@gmail.com>
2025-07-03 13:57:56 +0500

    24-dars useState Hook

--- sammi-app/src/components/app/App.js
+++ sammi-app/src/components/app/App.js
@@ -1,129 +1,79 @@
-import { Component } from "react";
+import { useState, Fragment } from "react";
 import "../app/App.css";
-import ContactUs from "../contact-us/contact-us";
-import { v4 as uuidv4 } from "uuid";
 
-// class User extends Component {
-//   constructor(pros) {
-//     super(pros);
-//     this.state = {
-//       count: 10,
-//       age: "",
-//     };
+const User = ({ firstName, lastName, link }) => {
+  // const [state, setState] = useState({
+  //   counter: 0,
+  //   isLogin: false,
+  // });
 
-//     // this.incrFun = this.incrFun.bind(this)
-//   }
+  // const onDecriment = () => {
+  //   setState(prevState => ({ ...prevState, counter: prevState.counter +1 }))
+  // };
 
-//   incrFun = () => {
-//     this.setState({
-//       count: this.state.count + 1,
-//     });
-//   }
+  // const onRestart = () => {};
 
-//   decFun = () => {
-//     this.setState({
-//       count: this.state.count - 1,
-//     });
-//   };
+  // const onToggLeLogin = () => {};
 
-//   zeroFun = () => {
-//     this.setState({
-//       count: (this.state.count = 0),
-//     });
-//   };
 
-//   changeHendlar = (e) => {
-//     this.setState({
-//       age: e.target.value,
-//     });
-//   };
+  const [counter, setCount] = useState(0);
+  const [isLogin, setLogin] = useState(false);
 
-//   render() {
-//     const { firstName, lastName, link } = this.props;
-//     const { age, count } = this.state;
-
-//     return (
-//       <div className="w-50 mx-auto">
-//         <div className="border p-3 mt-5">
-//           <h4>
-//             Mening ismim {firstName}, sharifim {lastName}, yoshim - {age}
-//           </h4>
-//           <a href={link}>Youtub kanalim</a>
-//           <div className="mt-5">
-//             <button
-//               type="button"
-//               className="btn btn-success btn-incr"
-//               onClick={this.incrFun}
-//             >
-//               incriment
-//             </button>
-//             <button
-//               type="button"
-//               className="btn btn-danger"
-//               onClick={this.decFun}
-//             >
-//               dec
-//             </button>
-//             <button
-//               type="button"
-//               className="btn btn-warning"
-//               onClick={this.zeroFun}
-//             >
-//               sbros
-//             </button>
-//           </div>
-//           <p className="coutn">{count}</p>
-
-//           <form>
-//             <span>Yoshingiz</span>
-//             <input
-//               type="text"
-//               className="form-control"
-//               onChange={this.changeHendlar}
-//             />
-//           </form>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-    };
-  }
-
-  addForm = (item) => {
-    this.setState(({ data }) => ({
-      data: [...data, { ...item, id: uuidv4() }],
-    }));    
+  const onIcrement = () => {
+    setCount((prevContere) => prevContere + 1);
+    setCount((prevContere) => prevContere + 1);
   };
 
-  onDelete = (id) => {
-    console.log(id);
-    
-    this.setState(({data}) => ({
-      data: data.filter((c) => c.id !== id),
-    }))
+  const onDecriment = () => {
+    setCount(counter - 1);
   };
 
-  
-  addFilter = () => {
-    this.setState(({data}) => ({
-      data: [...data].sort((a, b) => a.name.localeCompare(b.name))
-    }))
-  }
+  const onRestart = () => {
+    setCount(0);
+  };
 
-  render() {
-    return (
-      <div>
-        <ContactUs data={this.state.data} addForm={this.addForm} onDelete = {this.onDelete} addFilter ={this.addFilter}/>
+  const onToggLeLogin = () => {
+    setLogin((prevState) => !prevState);
+  };
+
+  return (
+    <div className="w-50 mx-auto">
+      <div className="border p-3 mt-5">
+        <h1>
+          Mening ismim {firstName}, sharifim - {lastName}
+        </h1>
+        <a href={link}>Youtube kanalim</a>
+        <p className="text-center">{counter}</p>
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-success" onClick={onIcrement}>
+            +
+          </button>
+          <button className="btn btn-danger mx-2" onClick={onDecriment}>
+            -
+          </button>
+          <button className="btn btn-dark mx-2" onClick={onRestart}>
+            0
+          </button>
+        </div>
+
+        {isLogin ? <p className="text-center mt-3">LOGIN</p> : null}
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-outline-primary" onClick={onToggLeLogin}>
+            TOGGLE
+          </button>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Fragment>
+      <User firstName={"Jony"} lastName={"Ravshanov"} link={"youtube.com"} />
+      <User firstName={"Jony"} lastName={"Ravshanov"} link={"youtube.com"} />
+    </Fragment>
+  );
+};
 
 export default App;
--- sammi-app/src/components/contact-us/contact-us.js
+++ sammi-app/src/components/contact-us/contact-us.js
@@ -1,112 +1,224 @@
-import "../contact-us/contact-us.css";
-import { Component } from "react";
-import ContactComent from "./contact-comment/contact-comment";
+// import "../contact-us/contact-us.css";
+// import { Component } from "react";
+// import ContactComent from "./contact-comment/contact-comment";
 
-class ContactUs extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      number: "",
-    };
-  }
+// class ContactUs extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: "",
+//       email: "",
+//       number: "",
+//     };
+//   }
 
-  chengeInput = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-      [e.target.email]: e.target.value,
-      [e.target.number]: e.target.value,
-    });
-  };
+//   // constructor() {
+//   //   super();
+//   //   this.state = {
+//   //     data: [],
+//   //   };
+//   //   this.maxId = 0
+//   // }
 
-  addFormHendlar = (e) => {
-    this.props.addForm({
-      name: this.state.name,
-      email: this.state.email,
-      number: this.state.number,
-    });
-    this.setState({
-      name: "",
-      email: "",
-      number: "",
-    });
-  };
+//   // addForm = (item) => {
+//   //   this.setState(({ data }) => ({
+//   //     data: [...data, { ...item, id: this.maxId++}],
+//   //   }));
+//   // };
 
-  render() {
-    const { name, email, number } = this.state;
-    const { data, onDelete, addFilter } = this.props;
+//   // onDelete = (id) => {
+//   //   console.log(id);
 
-    return (
-      <div className="center">
-        <div className="coment-box">
-          <h1 className="coment-box__title">Contact Us</h1>
-          <p>do you have a question?</p>
+//   //   this.setState(({data}) => ({
+//   //     data: data.filter((c) => c.id !== id),
+//   //   }))
+//   // };
 
-          <form action="">
-            <div className="all-input">
-              <div>
-                <input
-                  type="text"
-                  placeholder="Enter your name"
-                  className="form-control"
-                  name="name"
-                  onChange={this.chengeInput}
-                  value={name}
-                />
-              </div>
-              <div>
-                <input
-                  type="text"
-                  placeholder="Enter your email"
-                  className="form-control"
-                  name="email"
-                  onChange={this.chengeInput}
-                  value={email}
-                />
-              </div>
-              <div>
-                <input
-                  type="number"
-                  placeholder="Enter your number"
-                  className="form-control"
-                  name="number"
-                  onChange={this.chengeInput}
-                  value={number}
-                />
-              </div>
-            </div>
-            <div className="all-btns mt-3 mb-4">
-              <button
-                type="button"
-                className="btn btn btn-primary w-25"
-                onClick={this.addFormHendlar}
-              >
-                Add
-              </button>
-              <button
-                type="button"
-                className="btn btn btn-primary w-25"
-                onClick={addFilter}
-              >
-                Filter
-              </button>
-            </div>
-          </form>
+//   // addFilter = () => {
+//   //   this.setState(({data}) => ({
+//   //     data: [...data].sort((a, b) => a.name.localeCompare(b.name))
+//   //   }))
+//   // }
 
-          {data.map((item) => (
-            <ContactComent
-              name={item.name}
-              email={item.email}
-              number={item.number}
-              key={item.id}
-              onDelete={() => onDelete(item.id)}
-            />
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
+//   chengeInput = (e) => {
+//     this.setState({
+//       [e.target.name]: e.target.value,
+//       [e.target.email]: e.target.value,
+//       [e.target.number]: e.target.value,
+//     });
+//   };
 
-export default ContactUs;
+//   addFormHendlar = () => {
+//     this.props.addForm({
+//       name: this.state.name,
+//       email: this.state.email,
+//       number: this.state.number,
+//     });
+//     this.setState({
+//       name: "",
+//       email: "",
+//       number: "",
+//     });
+//   };
+
+//   render() {
+//     const { name, email, number } = this.state;
+//     const { data, onDelete, addFilter } = this.props;
+
+//     return (
+//       <div className="center">
+//         <div className="coment-box">
+//           <h1 className="coment-box__title">Contact Us</h1>
+//           <p>do you have a question?</p>
+
+//           <form action="">
+//             <div className="all-input">
+//               <div>
+//                 <input
+//                   type="text"
+//                   placeholder="Enter your name"
+//                   className="form-control"
+//                   name="name"
+//                   onChange={this.chengeInput}
+//                   value={name}
+//                 />
+//               </div>
+//               <div>
+//                 <input
+//                   type="text"
+//                   placeholder="Enter your email"
+//                   className="form-control"
+//                   name="email"
+//                   onChange={this.chengeInput}
+//                   value={email}
+//                 />
+//               </div>
+//               <div>
+//                 <input
+//                   type="number"
+//                   placeholder="Enter your number"
+//                   className="form-control"
+//                   name="number"
+//                   onChange={this.chengeInput}
+//                   value={number}
+//                 />
+//               </div>
+//             </div>
+//             <div className="all-btns mt-3 mb-4">
+//               <button
+//                 type="button"
+//                 className="btn btn btn-primary w-25"
+//                 onClick={this.addFormHendlar}
+//               >
+//                 Add
+//               </button>
+//               <button
+//                 type="button"
+//                 className="btn btn btn-primary w-25"
+//                 onClick={addFilter}
+//               >
+//                 Filter
+//               </button>
+//             </div>
+//           </form>
+
+//           {data.map((item) => (
+//             <ContactComent
+//               name={item.name}
+//               email={item.email}
+//               number={item.number}
+//               key={item.id}
+//               onDelete={() => onDelete(item.id)}
+//             />
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default ContactUs;
+// class User extends Component {
+//   constructor(pros) {
+//     super(pros);
+//     this.state = {
+//       count: 10,
+//       age: "",
+//     };
+
+//     // this.incrFun = this.incrFun.bind(this)
+//   }
+
+//   incrFun = () => {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   }
+
+//   decFun = () => {
+//     this.setState({
+//       count: this.state.count - 1,
+//     });
+//   };
+
+//   zeroFun = () => {
+//     this.setState({
+//       count: (this.state.count = 0),
+//     });
+//   };
+
+//   changeHendlar = (e) => {
+//     this.setState({
+//       age: e.target.value,
+//     });
+//   };
+
+//   render() {
+//     const { firstName, lastName, link } = this.props;
+//     const { age, count } = this.state;
+
+//     return (
+//       <div className="w-50 mx-auto">
+//         <div className="border p-3 mt-5">
+//           <h4>
+//             Mening ismim {firstName}, sharifim {lastName}, yoshim - {age}
+//           </h4>
+//           <a href={link}>Youtub kanalim</a>
+//           <div className="mt-5">
+//             <button
+//               type="button"
+//               className="btn btn-success btn-incr"
+//               onClick={this.incrFun}
+//             >
+//               incriment
+//             </button>
+//             <button
+//               type="button"
+//               className="btn btn-danger"
+//               onClick={this.decFun}
+//             >
+//               dec
+//             </button>
+//             <button
+//               type="button"
+//               className="btn btn-warning"
+//               onClick={this.zeroFun}
+//             >
+//               sbros
+//             </button>
+//           </div>
+//           <p className="coutn">{count}</p>
+
+//           <form>
+//             <span>Yoshingiz</span>
+//             <input
+//               type="text"
+//               className="form-control"
+//               onChange={this.changeHendlar}
+//             />
+//           </form>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
