8f65615d56bfaef8f072f3994e0e75f7c4986e5e
jony <ravshanovjony7@gmail.com>
2025-07-06 12:44:10 +0500

    28 dars useMemo

--- sammi-app/src/components/app/App.js
+++ sammi-app/src/components/app/App.js
@@ -1,114 +1,106 @@
-import { useState, Component, Fragment } from "react";
+import { useEffect, useState, Component, useCallback, act } from "react";
 import "../app/App.css";
 
-class User extends Component {
-  constructor(pros) {
-    super(pros);
-    this.state = {
-      count: 10,
-      age: "",
-    };
+// class User extends Component {
+//   constructor(pros) {
+//     super(pros);
+//     this.state = {
+//       count: 10,
+//       age: "",
+//     };
 
-    // this.incrFun = this.incrFun.bind(this)
-  }
+//     // this.incrFun = this.incrFun.bind(this)
+//   }
 
-  incrFun = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
+//   incrFun = () => {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   };
 
-  decFun = () => {
-    this.setState({
-      count: this.state.count - 1,
-    });
-  };
+//   decFun = () => {
+//     this.setState({
+//       count: this.state.count - 1,
+//     });
+//   };
 
-  zeroFun = () => {
-    this.setState({
-      count: (this.state.count = 0),
-    });
-  };
+//   zeroFun = () => {
+//     this.setState({
+//       count: (this.state.count = 0),
+//     });
+//   };
 
-  changeHendlar = (e) => {
-    this.setState({
-      age: e.target.value,
-    });
-  };
+//   changeHendlar = (e) => {
+//     this.setState({
+//       age: e.target.value,
+//     });
+//   };
 
-  componentDidMount() {
-    console.log("mounting");
-  }
+//   componentDidMount() {
+//     console.log("mounting");
+//   }
 
-  componentDidUpdate () {
-    document.title = `Counter: ${this.state.count}`
-    console.log("Updatate");
-    
-  }
+//   componentDidUpdate () {
+//     document.title = `Counter: ${this.state.count}`
+//     console.log("Updatate");
 
-  render() {
-    const { firstName, lastName, link } = this.props;
-    const { age, count } = this.state;
+//   }
 
-    return (
-      <div className="w-50 mx-auto">
-        <div className="border p-3 mt-5">
-          <h4>
-            Mening ismim {firstName}, sharifim {lastName}, yoshim - {age}
-          </h4>
-          <a href={link}>Youtub kanalim</a>
-          <div className="mt-5">
-            <button
-              type="button"
-              className="btn btn-success btn-incr"
-              onClick={this.incrFun}
-            >
-              incriment
-            </button>
-            <button
-              type="button"
-              className="btn btn-danger"
-              onClick={this.decFun}
-            >
-              dec
-            </button>
-            <button
-              type="button"
-              className="btn btn-warning"
-              onClick={this.zeroFun}
-            >
-              sbros
-            </button>
-          </div>
-          <p className="coutn">{count}</p>
+//   render() {
+//     const { firstName, lastName, link } = this.props;
+//     const { age, count } = this.state;
 
-          <form>
-            <span>Yoshingiz</span>
-            <input
-              type="text"
-              className="form-control"
-              onChange={this.changeHendlar}
-            />
-          </form>
-        </div>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className="w-50 mx-auto">
+//         <div className="border p-3 mt-5">
+//           <h4>
+//             Mening ismim {firstName}, sharifim {lastName}, yoshim - {age}
+//           </h4>
+//           <a href={link}>Youtub kanalim</a>
+//           <div className="mt-5">
+//             <button
+//               type="button"
+//               className="btn btn-success btn-incr"
+//               onClick={this.incrFun}
+//             >
+//               incriment
+//             </button>
+//             <button
+//               type="button"
+//               className="btn btn-danger"
+//               onClick={this.decFun}
+//             >
+//               dec
+//             </button>
+//             <button
+//               type="button"
+//               className="btn btn-warning"
+//               onClick={this.zeroFun}
+//             >
+//               sbros
+//             </button>
+//           </div>
+//           <p className="coutn">{count}</p>
+
+//           <form>
+//             <span>Yoshingiz</span>
+//             <input
+//               type="text"
+//               className="form-control"
+//               onChange={this.changeHendlar}
+//             />
+//           </form>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 // const User = ({ firstName, lastName, link }) => {
-//   // const [state, setState] = useState({
-//   //   counter: 0,
-//   //   isLogin: false,
-//   // });
-
-//   // const onDecriment = () => {
-//   //   setState(prevState => ({ ...prevState, counter: prevState.counter +1 }))
-//   // };
-
-//   // const onRestart = () => {};
-
-//   // const onToggLeLogin = () => {};
+//   const [state, setState] = useState({
+//     counter: 0,
+//     isLogin: false,
+//   });
 
 //   const [counter, setCount] = useState(0);
 //   const [isLogin, setLogin] = useState(false);
@@ -129,6 +121,12 @@ class User extends Component {
 //   const onToggLeLogin = () => {
 //     setLogin((prevState) => !prevState);
 //   };
+
+//   useEffect(() => {
+//     console.log('effect');
+
+//     document.title = `Counter ${counter}`
+//   }, [])
 
 //   return (
 //     <div className="w-50 mx-auto">
@@ -161,12 +159,59 @@ class User extends Component {
 //   );
 // };
 
+const bigCountNumber  = (number) => {
+  let i = 0;
+  while (i<100000) i++
+  return number * 2 
+}
+
+const User = () => {
+  const [counter, setCount] = useState(0);
+  const [active, setActive] = useState(true);
+
+  const onIcrement = () => {
+    setCount((preState) => preState + 1);
+  };
+
+  const onToggle = () => {
+    setActive((preState) => !preState);
+  };
+
+  // const counterGenerate = useCallback(
+  //   () =>
+  //     new Array(counter).fill("").map((_, idx) => `Counter number ${idx + 1}`),
+  //   [counter]
+  // );
+
+  const colors = {
+    fontWeight: 'bold',
+    color: active ? 'green': 'red'
+  }
+
+  const number = bigCountNumber(counter)
+
+  return (
+    <div className="w-50 mx-auto mb-5">
+      <div className="border p-3 mt-5">
+        <p className="text-center" style={colors}>user actived {number}</p>
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-success" onClick={onIcrement}>
+            Increase
+          </button>
+          <button className="btn btn-warning ml-3" onClick={onToggle}>
+            Toggle
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
-    <Fragment>
+    <>
       <User firstName={"Jony"} lastName={"Ravshanov"} link={"youtube.com"} />
-      <User firstName={"Jony"} lastName={"Ravshanov"} link={"youtube.com"} />
-    </Fragment>
+    </>
   );
 };
 
--- sammi-app/src/components/contact-us/contact-us.js
+++ sammi-app/src/components/contact-us/contact-us.js
@@ -1,141 +1,232 @@
-// import "../contact-us/contact-us.css";
-// import { Component } from "react";
-// import ContactComent from "./contact-comment/contact-comment";
+// // import "../contact-us/contact-us.css";
+// // import { Component } from "react";
+// // import ContactComent from "./contact-comment/contact-comment";
 
-// class ContactUs extends Component {
-//   constructor(props) {
-//     super(props);
+// // class ContactUs extends Component {
+// //   constructor(props) {
+// //     super(props);
+// //     this.state = {
+// //       name: "",
+// //       email: "",
+// //       number: "",
+// //     };
+// //   }
+
+// //   // constructor() {
+// //   //   super();
+// //   //   this.state = {
+// //   //     data: [],
+// //   //   };
+// //   //   this.maxId = 0
+// //   // }
+
+// //   // addForm = (item) => {
+// //   //   this.setState(({ data }) => ({
+// //   //     data: [...data, { ...item, id: this.maxId++}],
+// //   //   }));
+// //   // };
+
+// //   // onDelete = (id) => {
+// //   //   console.log(id);
+
+// //   //   this.setState(({data}) => ({
+// //   //     data: data.filter((c) => c.id !== id),
+// //   //   }))
+// //   // };
+
+// //   // addFilter = () => {
+// //   //   this.setState(({data}) => ({
+// //   //     data: [...data].sort((a, b) => a.name.localeCompare(b.name))
+// //   //   }))
+// //   // }
+
+// //   chengeInput = (e) => {
+// //     this.setState({
+// //       [e.target.name]: e.target.value,
+// //       [e.target.email]: e.target.value,
+// //       [e.target.number]: e.target.value,
+// //     });
+// //   };
+
+// //   addFormHendlar = () => {
+// //     this.props.addForm({
+// //       name: this.state.name,
+// //       email: this.state.email,
+// //       number: this.state.number,
+// //     });
+// //     this.setState({
+// //       name: "",
+// //       email: "",
+// //       number: "",
+// //     });
+// //   };
+
+// //   render() {
+// //     const { name, email, number } = this.state;
+// //     const { data, onDelete, addFilter } = this.props;
+
+// //     return (
+// //       <div className="center">
+// //         <div className="coment-box">
+// //           <h1 className="coment-box__title">Contact Us</h1>
+// //           <p>do you have a question?</p>
+
+// //           <form action="">
+// //             <div className="all-input">
+// //               <div>
+// //                 <input
+// //                   type="text"
+// //                   placeholder="Enter your name"
+// //                   className="form-control"
+// //                   name="name"
+// //                   onChange={this.chengeInput}
+// //                   value={name}
+// //                 />
+// //               </div>
+// //               <div>
+// //                 <input
+// //                   type="text"
+// //                   placeholder="Enter your email"
+// //                   className="form-control"
+// //                   name="email"
+// //                   onChange={this.chengeInput}
+// //                   value={email}
+// //                 />
+// //               </div>
+// //               <div>
+// //                 <input
+// //                   type="number"
+// //                   placeholder="Enter your number"
+// //                   className="form-control"
+// //                   name="number"
+// //                   onChange={this.chengeInput}
+// //                   value={number}
+// //                 />
+// //               </div>
+// //             </div>
+// //             <div className="all-btns mt-3 mb-4">
+// //               <button
+// //                 type="button"
+// //                 className="btn btn btn-primary w-25"
+// //                 onClick={this.addFormHendlar}
+// //               >
+// //                 Add
+// //               </button>
+// //               <button
+// //                 type="button"
+// //                 className="btn btn btn-primary w-25"
+// //                 onClick={addFilter}
+// //               >
+// //                 Filter
+// //               </button>
+// //             </div>
+// //           </form>
+
+// //           {data.map((item) => (
+// //             <ContactComent
+// //               name={item.name}
+// //               email={item.email}
+// //               number={item.number}
+// //               key={item.id}
+// //               onDelete={() => onDelete(item.id)}
+// //             />
+// //           ))}
+// //         </div>
+// //       </div>
+// //     );
+// //   }
+// // }
+
+// // export default ContactUs;
+
+// // import React, { useState, useCallback, useEffect } from "react";
+
+// // const App = () => {
+// //   const [count, setCount] = useState(0);
+
+// //   const handleClick = useCallback(() => {
+// //     console.log("Button clicked");
+// //   }, [count]); // 🔁 faqat count o'zgarganda funksiya qayta yaratiladi
+
+// //   useEffect(() => {
+// //     console.log("🔄 handleClick function recreated");
+// //   }, [handleClick]);
+
+// //   return (
+// //     <div className="text-center mt-5">
+// //       <p>Count: {count}</p>
+// //       <button onClick={handleClick} className="btn btn-outline-danger">Click Me</button>
+// //       <button onClick={() => setCount(count + 1)} className="btn btn-outline-dark">Callback</button>
+// //     </div>
+// //   );
+// // };
+
+// // export default App;
+
+
+// import React, { Component } from "react";
+
+// class Zadach extends Component {
+//   constructor() {
+//     super();
 //     this.state = {
-//       name: "",
-//       email: "",
-//       number: "",
+//       title: "",
 //     };
 //   }
 
-//   // constructor() {
-//   //   super();
-//   //   this.state = {
-//   //     data: [],
-//   //   };
-//   //   this.maxId = 0
-//   // }
+//   componentDidMount() {
+//     console.log("Mount");
+//   }
 
-//   // addForm = (item) => {
-//   //   this.setState(({ data }) => ({
-//   //     data: [...data, { ...item, id: this.maxId++}],
-//   //   }));
-//   // };
+//   componentWillUnmount() {
+//     console.log("Unmount");
+//   }
 
-//   // onDelete = (id) => {
-//   //   console.log(id);
-
-//   //   this.setState(({data}) => ({
-//   //     data: data.filter((c) => c.id !== id),
-//   //   }))
-//   // };
-
-//   // addFilter = () => {
-//   //   this.setState(({data}) => ({
-//   //     data: [...data].sort((a, b) => a.name.localeCompare(b.name))
-//   //   }))
-//   // }
-
-//   chengeInput = (e) => {
+//   setTitle = (e) => {
 //     this.setState({
-//       [e.target.name]: e.target.value,
-//       [e.target.email]: e.target.value,
-//       [e.target.number]: e.target.value,
+//       title: e.target.value,
 //     });
 //   };
 
-//   addFormHendlar = () => {
-//     this.props.addForm({
-//       name: this.state.name,
-//       email: this.state.email,
-//       number: this.state.number,
-//     });
-//     this.setState({
-//       name: "",
-//       email: "",
-//       number: "",
-//     });
-//   };
+//   addTitle = () => {
+//     document.title = this.state.title
+//   }
 
 //   render() {
-//     const { name, email, number } = this.state;
-//     const { data, onDelete, addFilter } = this.props;
-
 //     return (
-//       <div className="center">
-//         <div className="coment-box">
-//           <h1 className="coment-box__title">Contact Us</h1>
-//           <p>do you have a question?</p>
-
-//           <form action="">
-//             <div className="all-input">
-//               <div>
-//                 <input
-//                   type="text"
-//                   placeholder="Enter your name"
-//                   className="form-control"
-//                   name="name"
-//                   onChange={this.chengeInput}
-//                   value={name}
-//                 />
-//               </div>
-//               <div>
-//                 <input
-//                   type="text"
-//                   placeholder="Enter your email"
-//                   className="form-control"
-//                   name="email"
-//                   onChange={this.chengeInput}
-//                   value={email}
-//                 />
-//               </div>
-//               <div>
-//                 <input
-//                   type="number"
-//                   placeholder="Enter your number"
-//                   className="form-control"
-//                   name="number"
-//                   onChange={this.chengeInput}
-//                   value={number}
-//                 />
-//               </div>
-//             </div>
-//             <div className="all-btns mt-3 mb-4">
-//               <button
-//                 type="button"
-//                 className="btn btn btn-primary w-25"
-//                 onClick={this.addFormHendlar}
-//               >
-//                 Add
-//               </button>
-//               <button
-//                 type="button"
-//                 className="btn btn btn-primary w-25"
-//                 onClick={addFilter}
-//               >
-//                 Filter
-//               </button>
-//             </div>
-//           </form>
-
-//           {data.map((item) => (
-//             <ContactComent
-//               name={item.name}
-//               email={item.email}
-//               number={item.number}
-//               key={item.id}
-//               onDelete={() => onDelete(item.id)}
-//             />
-//           ))}
+//       <div>
+//         <h2>Component Created</h2>
+//         <button className="btn btn-danger mt-3" onClick={this.props.onDelete}>
+//           Delete
+//         </button>
+//         <div className="d-flex">
+//           <input
+//             type="text"
+//             className="d-block mt-3"
+//             placeholder="title"
+//             onChange={this.setTitle}
+//           />
 //         </div>
+//         <button className="btn btn-success mt-3" onClick={this.addTitle}>add title</button>
 //       </div>
 //     );
 //   }
 // }
 
-// export default ContactUs;
+// class App extends Component {
+//   state = {
+//     show: true,
+//   };
 
+//   handleDelete = () => {
+//     this.setState({ show: false });
+//   };
+
+//   render() {
+//     return (
+//       <div>{this.state.show && <Zadach onDelete={this.handleDelete} />}</div>
+//     );
+//   }
+// }
+
+// export default App;
